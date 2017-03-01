998539c4f7bc55825b0d1985faa7a7318048a4ad
deciemphysics <deciem.physics@gmail.com>
2017-03-01 16:10:24 -0700

    Finished game!

--- js-practice/monsterMunch.js
+++ js-practice/monsterMunch.js
@@ -2,12 +2,13 @@
 
 
 // create an array of food items for the monster to eat
-var foodarr = ["Apple","Baby","Banana","Donut","Treacle","Snickers","Doritos","MtDew","Coke","Pepsi"];
+var foodarr = ["Apple","Milk","Banana","Donut","Lettuce","Snickers","Doritos","MtDew","Coke","Pepsi", "Medicine", "Cranberry Juice"];
 var currentfood = "";
-
+var score = 0;
 // start the game with a function
 function rungame(){
     setfood(getfood());
+    updateScore();
 }
 
 // randomly select a food to put in the box
@@ -20,30 +21,44 @@ function getfood(){
 // put the food in the box
 function setfood(food){
     currentfood = food;
-    document.getElementById("foodbox").innerHTML = food;
+    if ( document.getElementById("foodbox").innerHTML == food){
+        rungame();
+    }
+    else {
+        document.getElementById("foodbox").innerHTML = food;
+    }
 }
-
+function updateScore(){
+    document.getElementById("scoreContent").innerHTML = score;
+}
 function eatit(){
     //if the food is good
-    if(currentfood == "Snickers" || currentfood == "Doritos" || currentfood == "MtDew"){
+    if(currentfood == "Snickers" || currentfood == "Doritos" || currentfood == "MtDew" || currentfood == "Donut" || currentfood == "Pepsi" || currentfood == "Coke"){
         document.getElementById("commentbox").innerHTML = "Yum-food!";
+        score += 1;
+        rungame();
     }
     // if the food is bad
     else{
-        document.getElementById("commentbox").innerHTML = "Me not feel so good.";
+        document.getElementById("commentbox").innerHTML = "Me want candy, not this!";
+        score -= 1;
+        rungame();
     }
 
 }
 
 function trashit(){
     //if the food is good
-    if(currentfood == "Apple" || currentfood == "Banana" || currentfood == "Pepsi"){
+    if(currentfood == "Apple" || currentfood == "Banana" || currentfood == "Milk" || currentfood == "Lettuce" || currentfood == "Medicine" || currentfood == "Cranberry Juice"){
         document.getElementById("commentbox").innerHTML = "Good! Me no want that!";
-
+        score += 1;
+        rungame();
     }
     // if the food is bad
     else{
         document.getElementById("commentbox").innerHTML = "Why you throw that away!?";
+        score -= 1;
+        rungame();
     }
 
 }
