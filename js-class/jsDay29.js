a00320fb3545a96e1dc18fbec701cc689126abb5
ronaldmarx <deciem.physics@gmail.com>
2017-03-30 15:30:06 -0600

    Notes on stack and datastructures

---
+++ js-class/jsDay29.js
@@ -0,0 +1,56 @@
+// DATA STRUCTURES
+//      A structure written in code that contains data.
+//      EX-- Array, object, etc
+// Hash table data structure!
+// Have a string as your key!
+//      ex students["Mick"]
+
+// STACK
+//      LIFO ( Last in first out )
+/*     
+       __________
+      | orange  |
+      ----------
+      | apple   |
+      ----------
+      | banana  |
+      ----------
+
+
+      We can use push, pop, and peek.
+      Push adds something to the top 
+      push("mango")
+
+       __________
+      | mango   |
+       __________
+      | orange  |
+      ----------
+      | apple   |
+      ----------
+      | banana  |
+      ----------
+
+      pop() // return "mango" and remove it from the stack
+    
+
+       __________
+      | orange  |
+      ----------
+      | apple   |
+      ----------
+      | banana  |
+      ----------
+
+      peek() // return orange without removing
+
+       __________
+      | orange  |
+      ----------
+      | apple   |
+      ----------
+      | banana  |
+      ----------
+
+      size() // return size of stack
+*/
